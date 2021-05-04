70ad22511f5afb421d1dd6375f81a08aa567933c
Mehmet <mehmetkusken@gmail.com>
2021-05-03 23:41:47 -0600

    commit tree 67e061edc645f0731cf5be12e73c53288dcfdf50

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
-
   # This is a sample static route.
   get '/hello' do
     "Hello World!"
@@ -9,10 +7,17 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "Hello #{params[:name]}!"
   end
 
-  # Code your final two routes here:
+	get "/goodbye/:name" do
+    "Goodbye, #{params[:name]}."
+  end
+
+  get '/multiply/:a/:b' do
+    "Result: #{params[:a].to_i * params[:b].to_i}"
+  end
+
+
 
 end
